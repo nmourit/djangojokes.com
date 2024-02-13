b03074323cf53b5603be1179b498195c2005557f
Nalee Mouritsen <nvang_1@hotmail.com>
2024-02-12 20:45:56 -0600

    updated DATABASES in settings.py to postgresql

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': '@Program123!',
+        'HOST': 'localhost',
+        'PORT': 5342
     }
 }
 
