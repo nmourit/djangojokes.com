64e91ef7822b47f12943f7bc2441a080e0a4694d
Nalee Mouritsen <nvang_1@hotmail.com>
2024-02-15 23:20:43 -0600

    Added Joke Tags

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Joke
+from .models import Category, Joke, Tag
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -16,6 +16,16 @@ class CategoryAdmin(admin.ModelAdmin):
 class JokeAdmin(admin.ModelAdmin):
     model = Joke
     list_display = ['question', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ['tag', 'created', 'updated']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
