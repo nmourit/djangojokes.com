70ab9edb43adfe76d707132cec1ec18c91ebef9b
Nalee Mouritsen <nvang_1@hotmail.com>
2024-02-17 17:01:07 -0600

    modified models.py

--- jokes/models.py
+++ jokes/models.py
@@ -11,9 +11,9 @@ class Joke(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.PROTECT
     )
     category = models.ForeignKey(
-        'Category', on_delete=models.PROTECT
+        'Category', on_delete=models.PROTECT, related_name='jokes'
     )
-    tags = models.ManyToManyField('Tag', blank=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='jokes')
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
     )
