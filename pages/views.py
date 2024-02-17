1aaa20a48e82766caabbe3cbe8773a0dcaea5038
Nalee Mouritsen <nvang_1@hotmail.com>
2024-02-16 18:48:24 -0600

    how to add display messages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
@@ -5,3 +7,12 @@ class HomePageView(TemplateView):
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+
+    # Only added get() method to learn how to add and display messages
+    """def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Debug message.')
+        messages.info(request, 'Info message.')
+        messages.success(request, 'Success message.')
+        messages.warning(request, 'Warning message.')
+        messages.error(request, 'Error message.')
+        return super().get(request, args, kwargs)"""
